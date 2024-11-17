1723eee33d6933a359928b9e42db96ba6494079a
Bareerah-zulquarnain <bareerah.zulquarnain@gmail.com>
2024-11-17 14:50:53 +0500

    Add files via upload

---
+++ Asymptotic-notation/second.py
@@ -0,0 +1,11 @@
+def Ontime (n):
+    iteration=0
+    for i in range (1, n+1):
+        iteration+=1 
+    print("when n is ",n,"iterations = " ,iteration )
+
+
+Ontime (10)
+Ontime (20)  
+print("\n with every'n'the time taken and iterations increase" )
+          
